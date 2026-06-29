4ca3e0d9d98971f0a05dd2dd907266f60ddacf3e
不知火 Shiranui <supersonic@livemail.tw>
2026-06-29 11:59:00 +0800

    feat: disable color mode to avoid dark mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,15 +4,17 @@ export default defineNuxtConfig({
     head: {
       titleTemplate: '%s - Web-Tech-TW',
       title: 'Wings',
-    }
+    },
   },
 
   compatibilityDate: '2025-07-15',
 
-  devtools: { enabled: true },
-  nitro: { preset: 'bun' },
+  devtools: {enabled: true},
+  nitro: {preset: 'bun'},
 
-  colorMode: { preference: 'light' },
+  ui: {
+    colorMode: false,
+  },
   css: ['~/assets/css/main.css'],
 
   modules: [
@@ -20,7 +22,7 @@ export default defineNuxtConfig({
     '@nuxt/image',
     '@nuxt/scripts',
     '@nuxt/ui',
-    '@nuxt/test-utils'
+    '@nuxt/test-utils',
   ],
 
   runtimeConfig: {
@@ -28,6 +30,6 @@ export default defineNuxtConfig({
       homeInteHost: 'https://web-tech.tw',
       saraInteHost: 'https://web-tech.tw/sara',
       saraTokenName: 'unified_token',
-    }
-  }
-})
+    },
+  },
+});
